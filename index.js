da0d11e900607dd0e5cf932baf3916b3b04860e5
Kaiju971 <129183625+Kaiju971@users.noreply.github.com>
2023-07-03 14:29:25 +0200

    V3

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+// document.querySelector('.hand').addEventListener('click', event => {
+// 	event.target.innerHTML = `✋`
+// 	document.querySelector('.mic').style = `--drop: 60vh; --spin: 900deg`
+// })
+// "use strict"
+// const lancerDe = function(){
+
+// 	const nombreDécimal = (Math.random()*6)+1
+// 	const nombre = Math.trunc (nombreDécimal)
+
+// 	return nombre
+// }
+// const clicSurBouton = Function(){
+// 	const resultat = lancerDe()
+
+// }
+
 document.querySelector('.hand').addEventListener('click', event => {
-	event.target.innerHTML = `✋`
-	document.querySelector('.mic').style = `--drop: 60vh; --spin: 900deg`
-})
+		event.target.innerHTML = `✋`
+		document.querySelector('.mic').style = `--drop: 60vh; --spin: 900deg`
+
+	})
+
+	let dé1 = 0;
+	let dé2 = 0;
+	function hand(){
+
+	}
